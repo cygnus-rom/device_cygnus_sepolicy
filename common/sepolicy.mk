5e657e47a3c9642bb70819cb7d47ccf7eed5e781
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:35:47 +0200

    sepolicy: Initial repo structure

---
+++ common/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from cygnus
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/cygnus/sepolicy/common
---
+++ qcom/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from cygnus
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/cygnus/sepolicy/qcom
