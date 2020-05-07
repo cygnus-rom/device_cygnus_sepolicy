5d9e9587959e38c49e0383fed2809b54235c8874
dianlujitao <dianlujitao@lineageos.org>
2020-05-07 22:43:01 +0800

    common: Expose aux camera {black,white}list props
    Change-Id: I8f6d2fcd498ed78d8404451b33ba830381c36061
    
    sepolicy: Dynamically build trust policy into system/vendor
    
     * Introduce a new board flag TARGET_USES_PREBUILT_VENDOR_SEPOLICY and
       a sepolicy variant: dynamic
     * When TARGET_USES_PREBUILT_VENDOR_SEPOLICY=true, dynamic act as
       private policy, and vendor policy is excluded in order to avoid
       conflicts (it's not integrated to final builds anyway). When the flag
       is not set, dynamic acts as vendor policy to survive from system
       image change i.e. GSI installation.
    
    Change-Id: I8bfd078d6064616c88e2c58a9fa3aa045dddf303

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,10 +3,22 @@
 # inherit from cygnus
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/cygnus/sepolicy/common/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/cygnus/sepolicy/common/dynamic
+endif
+
 BOARD_SEPOLICY_DIRS += \
+    device/cygnus/sepolicy/common/dynamic \
     device/cygnus/sepolicy/common/vendor
 
 # Selectively include legacy rules defined by the products
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,8 +3,16 @@
 # that inherit from cygnus
 #
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/conquer/sepolicy/qcom/private
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/cygnus/sepolicy/qcom/private
+
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/cygnus/sepolicy/qcom/vendor
+endif
