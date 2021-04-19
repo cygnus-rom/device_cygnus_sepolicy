lite-os/device_lite_sepolicy

sepolicy: Move policy to platform
Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c

common/sepolicy.mk
@@ -3,5 +3,8 @@
 # inherit from LiteOS
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/lite/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/lite/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lite/sepolicy/common/private

cygnus-rom/device_cygnus_sepolicy

common/sepolicy.mk
@@ -3,5 +3,5 @@
 # inherit from cygnus
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/cygnus/sepolicy/common
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/cygnus/sepolicy/common/private