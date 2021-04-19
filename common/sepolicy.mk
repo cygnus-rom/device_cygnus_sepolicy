xdroid-CAF/xd_device_xdroid_sepolicy

sepolicy: Move fingerprint 2.0 service to vendor sepolicy
Change-Id: Ifb55a009f4ff87b9a3571b24943b5e9c31f9dcd2

common/sepolicy.mk
@@ -5,3 +5,6 @@
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/conquer/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/conquer/sepolicy/common/vendor

cygnus-rom/device_cygnus_sepolicy

common/sepolicy.mk
@@ -5,3 +5,6 @@
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/cygnus/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/cygnus/sepolicy/common/vendor